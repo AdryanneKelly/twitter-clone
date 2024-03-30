85902f65e82ff4944ec8e7269bae3fd4425b3c6f
Adryanne Kelly <adryannekelly123@gmail.com>
2024-03-30 11:21:15 -0300

    adding infinite scroll on page

--- app/Livewire/Timeline.php
+++ app/Livewire/Timeline.php
@@ -9,11 +9,18 @@ class Timeline extends Component
 {
     // This will listen for the event `tweet::created` and refresh the component
     protected $listeners = ['tweet::created' => '$refresh'];
-    
+
+    public int $perPage = 10;
+
     public function render()
     {
         return view('livewire.timeline', [
-            'tweets' => Tweet::query()->latest()->get()
+            'tweets' => Tweet::query()->latest()->paginate($this->perPage)
         ]);
+    }
+
+    public function loadMore()
+    {
+        $this->perPage += 10;
     }
 }
--- resources/views/livewire/timeline.blade.php
+++ resources/views/livewire/timeline.blade.php
@@ -1,7 +1,28 @@
-<div class="mt-10 text-white text-lg">
-    @foreach($tweets as $tweet)
-    <div>
-        {{ $tweet->body }} 
-    </div>
+<div class="mt-10 text-white w-full text-lg">
+    <button wire:click='loadMore'>Load more</button>
+    @foreach ($tweets as $tweet)
+        <div>
+            {{ $loop->index }} - {{ $tweet->body }}
+        </div>
     @endforeach
+
+    <div id="jeremias" x-data="{
+        jeremias() {
+            const observer = new IntersectionObserver((entries) => {
+    
+                entries.forEach(entry => {
+                    if (entry.isIntersecting) {
+                        console.log('Intersecting');
+                        @this.loadMore();
+                    }
+                });
+            });
+            observer.observe(this.$el);
+        }
+    }" x-init="jeremias()">
+        <div wire:loading>
+            Loading...
+        </div>
+
+    </div>
 </div>
